24bef372b27deb13fc1664d2bfb7ed5bea0c8af0
davidy87 <dyoon0807@gmail.com>
2024-10-20 18:11:52 +0900

    정렬 알고리즘 문제 풀이:
    - 보물 (1026)

---
+++ sorting/보물.java
@@ -0,0 +1,33 @@
+package sorting;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class 보물 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+
+        List<Integer> A = new ArrayList<>(N);
+        List<Integer> B = new ArrayList<>(N);
+
+        for (String a : bf.readLine().replace("\n", "").split(" ")) {
+            A.add(Integer.parseInt(a));
+        }
+
+        for (String b : bf.readLine().replace("\n", "").split(" ")) {
+            B.add(Integer.parseInt(b));
+        }
+
+        Collections.sort(A);
+        Collections.sort(B, Collections.reverseOrder());
+
+        int result = IntStream.range(0, N)
+                .map(i -> A.get(i) * B.get(i))
+                .sum();
+
+        System.out.println(result);
+    }
+}
